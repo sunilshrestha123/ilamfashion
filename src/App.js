e1a48ce07ac09051761cc3c82649804bbd353a0c
Sunil Shrestha <ilamalisunilshrestha00@gmail.com>
2018-08-13 07:56:19 +0545

    hello react

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+        <div className="App-heading">
+<h1>New Fashion Collection</h1>
+</div>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
